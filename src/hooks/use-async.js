761b3c711e120c1f61d9f572d0a5a58d3ae4a747
Andrei Otea <otea@optymyze.com>
2019-02-11 14:20:08 +0200

    testing

--- src/hooks/use-async.js
+++
@@ -1,9 +0,0 @@
-import {useEffect, useState} from 'react';
-
-export default (fn, {watch = []} = {}) => {
-	const [data, setData] = useState();
-
-	useEffect(async () => setData(await fn()), watch);
-
-	return data;
-};
--- src/hooks/use-toggle.js
+++
@@ -1,8 +0,0 @@
-import {useState} from 'react';
-
-export default (initialState = false) => {
-	let [value, setValue] = useState(initialState),
-		toggle = () => value ? setValue(false) : setValue(true);
-
-	return [value, toggle]
-};
